411ee5e4e760c45b28e64d69f57a9070dc92ae8a
zuramai <saugi.dev@gmail.com>
2022-08-18 16:06:20 +0700

    a11y: fix low contrast footer text

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
-  <div class="container mx-auto opacity-50">
-    <p class="text-muted text-center text-sm "><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> 2021-PRESENT © Ahmad Saugi</p>
+  <div class="container mx-auto opacity-70">
+    <p class="text-gray-700 text-center text-sm ">2020-PRESENT © Ahmad Saugi</p>
   </div>
 </footer>
 </template>
