ca90fa0526c475bdd55d06318feb1b2a937526c0
zuramai <saugi.dev@gmail.com>
2022-08-18 14:40:47 +0700

    feat: contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -14,6 +14,25 @@ useHead({
   <div class="container mx-auto">
     <div class="prose">
       <h1 class="text-center">Reach Me!</h1>
+
+      <div class="cards max-w-[300px] mx-auto">
+        <div class="contact | flex items-center gap-3 mb-5">
+          <div class="i-mdi-gmail w-6 h-6 bg-red-500"></div>
+          <a href="saugi.dev@gmail.com">saugi.dev@gmail.com</a>
+        </div>
+        <div class="contact | flex items-center gap-3 mb-5">
+          <div class="i-mdi-telegram w-6 h-6 bg-[#2294cf]"></div>
+          <a href="https://t.me/zuramai" target="_blank">@zuramai</a>
+        </div>
+        <div class="contact | flex items-center gap-3 mb-5">
+          <div class="i-mdi-linkedin w-6 h-6 bg-blue-500"></div>
+          <a href="https://t.me/zuramai" target="_blank">Ahmad Saugi</a>
+        </div>
+        <div class="contact | flex items-center gap-3 mb-5">
+          <div class="i-mdi-instagram w-6 h-6 bg-red-500"></div>
+          <a href="https://instagram.com/asawgi" target="_blank">@asawgi</a>
+        </div>
+      </div>
     </div>
   </div>
 </section>
--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,9 @@ useHead({
     <div class="prose">
       <h1 class="text-center">Hi, Saugi's here!</h1>
       <p>
-        I am Ahmad Saugi, a bachelor of computer science student. My passion for software lies with dreaming up ideas and making them come true with elegant interfaces. I take great care in experience, architecture, and code quality of the things I build.
+        I am Ahmad Saugi, a bachelor of computer science student.
       </p>
+      <p>My passion for software lies with dreaming up ideas and making them come true with elegant interfaces. I take great care in experience, architecture, and code quality of the things I build.</p>
       <p>
         I am also an open-source enthusiast and maintainer. You can see <nuxt-link to="projects">list of my projects here</nuxt-link>. I love how collaboration and knowledge sharing happens through open-source and I am happy to see what I do could eventually feedback to the community and industry.
       </p>
