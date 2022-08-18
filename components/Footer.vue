53888bbead60e42480db1f6c2d312da28cb4d5ab
zuramai <saugi.dev@gmail.com>
2022-08-18 16:13:46 +0700

    a11y: add button label

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 <footer>
-  <div class="container mx-auto opacity-70">
+  <div class="container mx-auto ">
     <p class="text-gray-700 text-center text-sm ">2020-PRESENT © Ahmad Saugi</p>
   </div>
 </footer>
