efab1477226dc9a2e2282b9872963e5f456fe5bb
zuramai <saugi.dev@gmail.com>
2022-11-18 09:41:05 +0700

    ci: use pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,39 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt/config'
 import transformerDirective from '@unocss/transformer-directives'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: [
-    '@unocss/nuxt',
+    ['@unocss/nuxt', {
+      uno: true,
+      icons: true,
+      attributify: true,
+
+      // core options
+      shortcuts: [],
+      transformers: [
+        transformerDirective(),
+      ],
+      rules: [],
+      theme: {
+        colors: {
+          'heading': '#2E2E2E',
+          'subheading': '#16221C',
+          'body': '#555555'
+        }
+      },
+    }],
     '@pinia/nuxt',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Lexend: true,
+        Inter: [400],
+      }
+    }]
   ],
   // Global page headers (https://go.nuxtjs.dev/config-head)
- 
-  buildModules: [
-    '@nuxtjs/google-fonts'
-  ],
   css: [
     'normalize.css',
     'assets/css/style.css',
   ],
-  unocss: {
-    uno: true, 
-    icons: true, 
-    attributify: true, 
-
-    // core options
-    shortcuts: [],
-    transformers: [
-      transformerDirective(),
-    ],
-    rules: [],
-    theme: {
-      colors: {
-        'heading': '#2E2E2E',
-        'subheading': '#16221C',
-        'body': '#555555'
-      }
-    },
-  },
-  googleFonts: {
-    families: {
-      Lexend: true,
-      Inter: [400],
-    }
-  }
 })
