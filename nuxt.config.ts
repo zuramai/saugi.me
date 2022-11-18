9ca8dfd4c50a75f0f25c3c2849116128ab87c4d3
zuramai <saugi.dev@gmail.com>
2022-11-18 09:46:34 +0700

    chore: nitro preset netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import transformerDirective from '@unocss/transformer-directives'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'netlify'
+  },
   modules: [
     ['@unocss/nuxt', {
       uno: true,
