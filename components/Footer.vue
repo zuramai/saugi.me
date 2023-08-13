bb559105dcc381f656465f65f9ae50432604e5b1
zuramai <saugi.dev@gmail.com>
2023-08-13 17:46:19 +0700

    fix: footer margin

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
   <div class="container mx-auto ">
-    <p class="text-gray-700 text-center text-sm ">2020-PRESENT © Ahmad Saugi</p>
+    <p class="text-center text-sm my-0 py-5">2020-PRESENT © Ahmad Saugi</p>
   </div>
 </footer>
 </template>
