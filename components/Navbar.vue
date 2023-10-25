912035df6df592d91746ac9760bae8d7cba5df40
zuramai <saugi.dev@gmail.com>
2023-10-25 09:55:09 +0700

    fix: navbar lightbulb placing

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -18,14 +18,18 @@ const toggleDark = useToggle(isDark)
 </script>
 <template>
   <nav>
-    <div class="navbar__brand | italic text-center flex justify-center py-3">
+    <div class="navbar__brand | italic text-center flex justify-center py-3 px-8">
       <div class='w-1/3'></div>
-      <h3 class="w-1/3 text-xl my-0">
+      <h3 class="w-1/3 text-xl my-0 flex items-center justify-center">
         <router-link to="/" class="decoration-none | dark:text-white!">Saugi</router-link>
       </h3>
-      <div class='w-1/3'>
+      <div class='w-1/3 flex justify-end gap-3 items-center'>
+        <a href="#" class="navbar__link | p-2! md:hidden block" @click.prevent="toggleDark()">
+          <div class="text-xl i-mdi-weather-night" v-if="isDark == true"></div>
+          <div class="text-xl i-mdi-lightbulb-on-outline" v-else></div>
+        </a>
         <button class="p-2 bg-transparent border-none md:hidden inline-block" type="button" name="toggle-menu" aria-label="Toggle Menu" @click="isNavbarOpen=!isNavbarOpen">
-          <div class="i-mdi-menu"></div>
+          <div class="i-mdi-menu text-xl"></div>
         </button>
       </div>
     </div>
@@ -47,7 +51,7 @@ const toggleDark = useToggle(isDark)
           <nuxt-link to="/contact" class="navbar__link">Reach me!</nuxt-link>
         </li>
         <li class="navbar__item ">
-          <a href="#" class="navbar__link | p-0!" @click.prevent="toggleDark()">
+          <a href="#" class="navbar__link | p-0! hidden md:block" @click.prevent="toggleDark()">
             <div class="i-mdi-weather-night" v-if="isDark == true"></div>
             <div class="i-mdi-lightbulb-on-outline" v-else></div>
           </a>
